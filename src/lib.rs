0c051c61846cdb668e171f19f28cfcdf734f2867
nester-a <nester.a.l@yandex.ru>
2025-11-15 16:57:30 +0300

    task complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashSet;
+
+pub fn analyze_text(text: &str) -> (usize, usize, String) {
+    let mut count: usize = 0;
+    let mut uniq_words: HashSet<String> = HashSet::new();
+    let mut longest: &str = "";
+
+    for word in text.split_whitespace() {
+        count = count + 1;
+
+        if word.len() > longest.len() {
+            longest = word;
+        }
+
+        uniq_words.insert(word.to_lowercase());
+    }
+
+    (count, uniq_words.len(), String::from(longest))
 }
 
 #[cfg(test)]
@@ -8,7 +24,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let text = "hello world hello again";
+        assert_eq!(analyze_text(text), (4, 3, "hello".to_string()));
     }
 }
